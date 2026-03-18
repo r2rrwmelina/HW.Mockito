c9784393b58b89d8285a29743ec238a0fef2d116
MG <gmelina69@gmail.com>
2026-03-18 23:27:41 +0400

    Создать класс для объекта строки - название фильма
    Создание конструктора для объекта
    Создание метода getter() для обращения к объекту

---
+++ src/main/java/ru/netology/domain/MoviesTitles.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class MoviesTitles {
+    private String movieName;
+
+    public MoviesTitles(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovie() {
+        return movieName;
+    }
+}
